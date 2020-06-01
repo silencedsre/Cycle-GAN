7c1d0e6693c52c47360e7eee81760fec3c6b2deb
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-01 13:42:46 +0545

    send prediction image

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 from flask import Flask, request
 from flask_cors import CORS
 import json
+from PIL import Image
 from src.cyclegan import *
-from flask import send_file
+from flask import send_file, url_for
 
 app = Flask(__name__)
 
 # Allow
 CORS(app)
 
+def convert_image(file):
+    img = Image.open(file)
+    img = np.array(img)
+    img = tf.convert_to_tensor(
+        img, dtype=None, dtype_hint=None, name=None
+    )
+
+    img = tf.reshape(tf.cast(tf.image.resize(img, (int(img_rows), int(img_cols))), tf.float32) / 127.5 - 1,
+               (1, img_rows, img_cols, channels))
+
+    # img = tf.image.convert_image_dtype(img, tf.float32)
+    # img = tf.image.resize(img, [img_cols, img_rows])
+    # img = tf.expand_dims(img, axis=0)
+    return img
+
 
 def load_model():
     generator_g = generator()
     generator_f = generator()
 
-    generator_g.load_weights('./temp/models/generator_g.h5')
-    generator_f.load_weights('./temp/models/generator_f.h5')
+    generator_g.load_weights('./src/temp/models/generator_g.h5')
+    generator_f.load_weights('./src/temp/models/generator_f.h5')
 
     return generator_g, generator_f
 
@@ -32,19 +48,19 @@ def monet_to_photo():
     if request.method == 'POST':
         print("request files", request.files)
         file = request.files['file']
-        img = preprocess(img=file)
+        img = convert_image(file=file)
         img = img.numpy()
         print(f"Shape of image {img.shape}")
         generator_g, generator_f = load_model()
         y_hat = generator_g.predict(img.reshape((1, img_rows, img_cols, channels)))
 
-        #todo save y_hat as png or sth
-        if request.args.get('type') == '1':
-            filename = 'ok.gif'
-        else:
-            filename = 'error.gif'
-        return send_file(filename, mimetype='image/gif')
-
+        y_hat = (y_hat)*255
+        y_hat = y_hat[0].astype(np.uint8)
+        im = Image.fromarray(y_hat)
+        filename = time.time()
+        im.save(f"./static/{filename}.jpg")
+        filename = f"{filename}.jpg"
+        return url_for('static', filename=filename)
 
 
 @app.route('/photo_to_monet', methods=['GET', 'POST'])
